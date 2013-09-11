465c79bd37c789981a68de824a9501543f4931a3
lfc <bayinamine@gmail.com>
2013-09-11 16:43:50 +0800

    refactor

--- src/robot.erl
+++ src/robot.erl
@@ -11,7 +11,8 @@ start() ->
   {ok, Context} = erlzmq:context(),
   {ok, Socket} = erlzmq:socket(Context, dealer),
   ok = erlzmq:setsockopt(Socket, identity, pid_to_list(self())),
-  ServerAddr = "tcp://10.10.9.164:5570",
+%%   ServerAddr = "tcp://10.10.9.164:5570",
+  ServerAddr = "tcp://127.0.0.1:5570",
   ok = erlzmq:connect(Socket, ServerAddr),
   io:format("connect to ~s success.~n", [ServerAddr]),
   login(Socket),
@@ -24,13 +25,17 @@ login(Socket) ->
 %%   io:format("TransUnit: ~w~n", [TransUnit]),
   Msg = list_to_binary(rpc_pb:encode_transunit(TransUnit)),
   erlzmq:send(Socket, Msg),
+  loop(Socket).
+
+loop(Socket) ->
   case polling(Socket, 100, 10) of
     {error, timeout} ->
       io:format("Error: Received timeout.~n");
     {ok, ReplyBin} ->
       #transunit{'$extensions' = Extensions} = rpc_pb:decode_transunit(ReplyBin),
       io:format("ReplyMsg: Extensions= ~w~n", [Extensions])
-  end.
+  end,
+  loop(Socket).
 
 polling(_Socket, 0, _Delay) ->
   {error, timeout};
