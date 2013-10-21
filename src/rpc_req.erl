84853f0b13c33d70b91da1ccf3846a9b5271b30e
lfc <bayinamine@gmail.com>
2013-10-21 14:40:35 +0800

    bugfix

--- src/rpc_req.erl
+++ src/rpc_req.erl
@@ -27,7 +27,7 @@ create_account_req(RobotId) ->
 
 login_req(RobotId) ->
   Id = integer_to_list(RobotId),
-  DeviceId = "!@#-robot-" ++ Id,
+  DeviceId = "robot-!@#-" ++ Id,
   Message = #loginrequest{device_id = DeviceId, client_version = "0.5.0", meta_crc32 = "8414FF96"},
   wrap_transunit(Message).
 
